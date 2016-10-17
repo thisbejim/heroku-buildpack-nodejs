60caf4d0a37b832ddff52472bf28e62a877adf4d
James Childs-Maidment <jimmy_cm3@hotmail.com>
2016-10-17 14:52:22 +1100

    test

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -12,7 +12,7 @@ install_docker() {
   local download_url="https://get.docker.com/"
   local code=$(curl https://get.docker.com/ --silent --fail --retry 5 --retry-max-time 15)
   if [ "$code" != "200" ]; then
-    echo "Unable to download node $version; does it exist?" && false
+    echo "Unable to download docker does it exist?" && false
   fi
 }
 
